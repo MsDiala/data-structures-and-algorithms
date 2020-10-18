8220b6f71be6519044a721ac01436ba0af0d02a6
MsDiala <diala-is-diala@hotmail.com>
2020-10-19 01:19:32 +0300

    for

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -2,9 +2,7 @@
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1
-
 Write a function named greeting that takes in a string and returns the string in all uppercase letters.
-
 Then, write a function named speaker that takes in a string and a callback function. The speaker function should return the string in all uppercase letters only by invoking the callback.
 ------------------------------------------------------------------------------------------------ */
 
@@ -20,17 +18,13 @@ const speaker = (message, callback) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
-
 Write a function named addValues that takes in an array and a value and pushes the value into the array. This function does not need a return statement.
-
 Then, write a function named addNumbers that takes in four arguments:
   - A number to be added to an array
   - An array into which the number should be added
   - The number of times the number should be added
   - A callback function to use to add the numbers to the array (Hint: you already defined it)
-
 Within the addNumbers function, invoke the callback function as many times as necessary, based on the third argument of the addNumbers function.
-
 Return the modified array.
 ------------------------------------------------------------------------------------------------ */
 
@@ -42,15 +36,12 @@ const addNumbers = (num, arr, times, callback) => {
   for(let i=0; i<times; i++){
     callback(arr, num);
   }
-  return arr;
+  return arr;   
 };
 
 /* ------------------------------------------------------------------------------------------------
-
 CHALLENGE 3
-
 Write a function named createList that takes in an array of the current store intentory.
-
 The inventory is formatted like this:
 [
   { name: 'apples', available: true },
@@ -59,7 +50,6 @@ The inventory is formatted like this:
   { name: 'bananas', available: true },
   { name: 'blueberries', available: false }
 ]
-
 This function should use forEach to populate your grocery list based on the store's inventory. If the item is available, add it to your list. Return the final list.
 ------------------------------------------------------------------------------------------------ */
 
@@ -77,15 +67,12 @@ const createList = (availableItems) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
-
 Write a function named fizzbuzz that takes in an array of numbers.
-
 Iterate over the array using forEach to determine the output based on several rules:
   - If a number is divisible by 3, add the word "Fizz" to the output array.
   - If the number is divisible by 5, add the word "Buzz" to the output array.
   - If the number is divisible by both 3 and 5, add the phrase "Fizz Buzz" to the output array.
   - Otherwise, add the number to the output array.
-
 Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
@@ -108,15 +95,12 @@ const fizzbuzz = (arr) => {
   });
   return outputArray;
 }
+
 /* ------------------------------------------------------------------------------------------------
 TESTS
-
 All the code below will verify that your functions are working to solve the challenges.
-
 DO NOT CHANGE any of the below code.
-
 Run your tests from the console: jest challenges-01.test.js
-
 ------------------------------------------------------------------------------------------------ */
 
 describe('Testing challenge 1', () => {
